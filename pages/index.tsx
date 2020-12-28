719f7ae2f81cefcab4b743041edc780ad1bffecb
mk422756 <k422756+git@gmail.com>
2020-12-28 23:44:26 +0900

    トップページ修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,34 @@ import {useState, useEffect} from 'react'
 import Layout from 'layouts/layout'
 import {getCollections} from 'clients/collection'
 import CollectionTile from 'components/CollectionTile'
+import Image from 'next/image'
+import Button from 'components/Button'
+
+const LandingBox = () => {
+  return (
+    <div className="bg-white p-4">
+      <p className="text-2xl text-center mt-4 font-semibold">
+        問題集を探してみよう！
+      </p>
+      <p className="text-lg text-center mt-4 font-semibold">
+        必要な問題集を見つけて、
+        <br />
+        効率的に学習を始めましょう！
+      </p>
+      <div className="text-center mt-4">
+        <Image
+          src="/landing.png"
+          alt="landing image"
+          width={209}
+          height={143}
+        ></Image>
+      </div>
+      <div className="text-center my-4">
+        <Button>SQUIZとは？</Button>
+      </div>
+    </div>
+  )
+}
 
 export default function Home() {
   const [collections, setCollections] = useState([])
@@ -22,14 +50,19 @@ export default function Home() {
   return (
     <Layout>
       <main>
-        {collections.map((collection) => {
-          return (
-            <CollectionTile
-              key={collection.id}
-              collection={collection}
-            ></CollectionTile>
-          )
-        })}
+        <LandingBox></LandingBox>
+        <div className="mt-2">
+          {collections.map((collection) => {
+            return (
+              <div className="mt-1">
+                <CollectionTile
+                  key={collection.id}
+                  collection={collection}
+                ></CollectionTile>
+              </div>
+            )
+          })}
+        </div>
       </main>
     </Layout>
   )
